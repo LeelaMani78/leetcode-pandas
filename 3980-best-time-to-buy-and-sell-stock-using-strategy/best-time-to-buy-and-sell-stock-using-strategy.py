f00ea6da66aae39787ff9f5c8d595b7076c548f8
leela mani <leelaleelamani97@gmail.com>
2025-12-18 11:54:53 -0600

    Time: 267 ms (75.68%) | Memory: 34 MB (66.54%) - LeetSync

---
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
+        prefix_sell = [0]
+        prefix = [0]
+
+        for p, s in zip(prices, strategy):
+            prefix.append(prefix[-1] + p * s)
+            prefix_sell.append(prefix_sell[-1] + p)
+
+        N = len(prices)
+        total = prefix[-1]
+        best = total
+
+        for i in range(N):
+            if i + k <= N:
+                best = max(
+                    best,
+                    total
+                    - (prefix[i + k] - prefix[i])
+                    + (prefix_sell[i + k] - prefix_sell[i + k // 2])
+                )
+
+        return best
